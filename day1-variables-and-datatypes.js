3b0d1cf3952c77458861eb15d58e792df8fafee6
shivaram <thallojushiva@gmail.com>
2024-07-14 10:02:50 +0530

    completed 2 days of coding task

---
+++ day1-variables-and-datatypes.js
@@ -0,0 +1,47 @@
+        // activity-1 variables and data types
+
+        var firstNumber = 12;
+        console.log(firstNumber)
+        let firstName = 'Shivaram'
+        console.log(firstName);
+
+        //activity-2 const declaration
+
+        const firsrtBooleanValue = 10.5;
+        console.log(firsrtBooleanValue)
+
+        //activity-3 data-types
+
+        let secondNumber = 23;
+        console.log(typeof secondNumber)
+
+        let secondName = 'vittu'
+        console.log(typeof secondName);
+
+        let secondBooleanValue = 34.56;
+        console.log(typeof secondBooleanValue)
+
+        let firstObject = {
+            firstName: 'shivaram',
+            lastName: 'thalloju',
+            phoneNumber: 12323,
+        }
+        console.log(typeof firstObject)
+
+        let firstArray = [1,'prabhas', 23.5, [2,3,'chiti'], 'animal']
+        console.log( typeof firstArray);
+
+
+
+        // activity-4 reassinging variables
+        let thirdName = 'kajal'
+        console.log(thirdName);
+        thirdName = 'RCB'
+        console.log(thirdName)
+
+        //activity-5 understanding const
+        const forthName = 'KALKI2898AD'
+        console.log(forthName)
+        // forthName = 'salaar';
+        // console.log(forthName);
+        
---
+++ day2-operators.js
@@ -0,0 +1,79 @@
+        //activity-1 airthmetic operations
+
+        //add two numbers
+        let firstNumber = 10;
+        let secondNumber = 15;
+        console.log(firstNumber + secondNumber)
+
+        //subtract two numbers
+        let thirdNumber = 12;
+        let fourthNumber = 13
+        console.log(thirdNumber - fourthNumber);
+
+        //multiply two numbers
+        let fifthNumber = 12;
+        let sixthNumber = 13
+        console.log( fifthNumber * sixthNumber)
+
+        //divide two numbers
+        let seventhNumber = 23;
+        let eigthNumber = 14;
+        console.log(seventhNumber/eigthNumber)
+
+        //find the remainder
+        let ninethNumber = 16;
+        let tenthNumber = 2;
+        console.log(ninethNumber % tenthNumber)
+
+
+
+        //activity-2 assignment operators
+        let eleventhNumber = 34
+        eleventhNumber += 2
+        console.log(eleventhNumber)
+
+        let twelvethNumber = 23
+        twelvethNumber -= 3;
+        console.log(twelvethNumber)
+
+        //activity-3 comparsion operator
+        let thirteenNumber = 24;
+        let fourteenNumber = 12;
+        console.log(thirteenNumber > fourteenNumber)
+
+        let fifteenNumber = 23;
+        let sixteenNumber = 54;
+        console.log(fifteenNumber < sixteenNumber)
+
+        let seventeenNumber = 54;
+        let eighteenNumber = 34;
+        console.log(seventeenNumber >= eighteenNumber)
+
+        let ninteenNumber = 45;
+        let twentyNumber = 12;
+        console.log(ninteenNumber <= twentyNumber)
+
+        let twentyOneNumber = '23'
+        let twentyTwoNumber = 23;
+        console.log(twentyOneNumber == twentyTwoNumber)
+
+        let twentyThreeNumber = 32
+        let twentyFourNumber = '32'
+        console.log(twentyThreeNumber === twentyFourNumber)
+
+        //activity-4 logical operators
+        console.log('logical and')
+        console.log( 55>22 && 34>23)
+        console.log('logical OR')
+        console.log(23>34 || 10>1)
+
+        //not operator
+        console.log('not operator')
+        let twentyFiveNumber = true
+        console.log(!twentyFiveNumber)
+
+        //activity-5 ternaryoperator
+        console.log('teranary operator')
+        console.log(13%2 ==0 ?'even':'odd')
+
+
